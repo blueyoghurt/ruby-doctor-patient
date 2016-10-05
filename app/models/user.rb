077cb6c08e1f89c2dc82ba1649f7adc9c31edb62
Corine Lim <blue.yoghurt@gmail.com>
2016-10-05 17:14:26 +0800

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :appointments
   has_many :doctors, :through => :appointments
+
+  validates :password, length: { in: 8..72 }, on: :create
+
+  validates :email,
+  presence: true,
+  uniqueness: {case_sensitive: false}
+
+  has_secure_password
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
+
 end
