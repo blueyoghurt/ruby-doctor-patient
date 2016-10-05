a3c181a0ccfbdd8d6106188ec37803dea411d214
Corine Lim <blue.yoghurt@gmail.com>
2016-10-05 20:53:44 +0800

    added minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 # Doctor.create({"name"=>"Belgien"})
 Doctor.create({"name"=>"Luke", "speciality"=>"Brain & Nerves (Neurology)","contact" =>12345678})
 Doctor.create({"name"=>"Wei Ying", "speciality"=>"Bones (Orthopaedics)","contact" =>222222222})
 Doctor.create({"name"=>"Dexter", "speciality"=>"Cancer (Oncology)","contact" =>33333333})
 Doctor.create({"name"=>"Mr.Dog", "speciality"=>"Ear, Nose & Throat (Otorhinolaryngology)","contact" =>44444444})
 Doctor.create({"name"=>"Mr.Cat", "speciality"=>"Heart & Vascular (Cardiovascular)","contact" =>5555555})
-# Appointment_time.create({"time"=>"9 am"})
 
-
-
-User.find_or_create_by!(email: 'jeremiah@fakeemail.com') do |user|
+User.find_or_create_by!(email: 'test@test.com') do |user|
   user.first_name = 'Jeremiah'
   user.last_name = 'Alexander'
   user.is_admin = true
   user.password = "password"
-  # user.password_confirmation = "password"
+  user.contact = 12345678
 end
-
-puts "File working but cant create data"
