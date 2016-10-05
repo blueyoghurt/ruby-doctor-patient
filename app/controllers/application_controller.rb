e74bc1d3522b79ef336a5034181eedc8898eca3c
Corine Lim <blue.yoghurt@gmail.com>
2016-10-05 21:28:13 +0800

    index user page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
 
+  # authentication is true, if we have a current_user
+  def is_authenticated
+    unless current_user
+      flash[:danger] = "User must be logged in!"
+      redirect_to login_path
+    end
+  end
+
+  def is_authenticated_admin
+    unless current_user.is_admin?
+      flash[:danger] = "Admin Only Access!"
+      redirect_to root_path
+    end
+  end
+
   # Do we have a @current_user variable? if not try to find a user in the db using the session id
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
