7277360c672d8c1ec15b90ad79d9824c5860ef69
Corine Lim <blue.yoghurt@gmail.com>
2016-10-05 20:07:14 +0800

    added bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create({"first_name"=>"Mr.Admin", "last_name"=>"Low","email" =>"mradmin@gmail.com","password_digest" =>"12345678","contact" =>"12345678","is_admin" =>TRUE})
+
+User.find_or_create_by!(email: 'jeremiah@fakeemail.com') do |user|
+  user.first_name = 'Jeremiah'
+  user.last_name = 'Alexander'
+  user.is_admin = true
+  user.password = "password"
+  # user.password_confirmation = "password"
+end
+
+puts "Finished DB seeding."
