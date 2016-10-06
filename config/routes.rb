5eb631531426fbf418987752e796ced4d2c994fa
Luke <blue.yoghurt@gmail.com>
2016-10-06 14:09:27 +0800

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
   resources :appointment_times
   resources :appointments
   resources :doctors
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
